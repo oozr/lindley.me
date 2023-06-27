dd55d28f39246febc38926c5b49432e19043c108
George <george.j.lindley@gmail.com>
2023-06-27 10:17:43 +0300

    with CEFR analyser

--- helpers.py
+++ helpers.py
@@ -1,3 +1,5 @@
+import textstat
+
 def calculate_coleman_liau_index(text):
     # Add the Coleman-Liau index calculation code here
     char_count = len([char for char in text if char.isalnum()])
@@ -8,3 +10,20 @@ def calculate_coleman_liau_index(text):
     score = round(0.0588 * L - 0.296 * S - 15.8, 2)
 
     return score
+
+def estimate_cefr_level(text):
+    # calculate the Flesch-Kincaid Grade Level of the text
+    fkg = textstat.flesch_kincaid_grade(text)
+    # convert the Flesch-Kincaid Grade Level to a CEFR level using the CEFR scale
+    if fkg <= 5:
+        cefr = "A2"
+    elif fkg <= 6:
+        cefr = "B1"
+    elif fkg <= 7:
+        cefr = "B2"
+    elif fkg <= 8:
+        cefr = "C1"
+    else:
+        cefr = "C2"
+    
+    return cefr
--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from helpers import calculate_coleman_liau_index
+from helpers import calculate_coleman_liau_index, estimate_cefr_level
 
 views = Blueprint('views', __name__)
 
@@ -11,15 +11,25 @@ def home():
 def blog():
     return render_template("blog.html")
 
-@views.route('/text_analyzer', methods = ["GET", "POST"])
-def text_analyzer():
+@views.route('/gse_analyser', methods = ["GET", "POST"])
+def gse_analyser():
     if request.method == "POST":
         data = request.form['text']
         # Calculate the Coleman-Liau index, formula found in helpers.py
         score = calculate_coleman_liau_index(data)
         grade = int(round(score))
-        return render_template('result.html', score=score, grade=grade)  
-
-
+        cefr = estimate_cefr_level(data)
+        return render_template('gse_result.html', score=score, grade=grade, cefr=cefr)  
     #Otherwise a GET request will just render the HTML
-    return render_template("text_analyzer.html")
+    return render_template("gse_analyser.html")
+
+@views.route('/cefr_analyser', methods = ["GET", "POST"])
+def cefr_analyser():
+    if request.method == "POST":
+        data = request.form['text']
+        language = request.form['language']
+        # Calculate the Coleman-Liau index, formula found in helpers.py
+        cefr = estimate_cefr_level(data)
+        return render_template('cefr_result.html', cefr=cefr, language=language)  
+    #Otherwise a GET request will just render the HTML
+    return render_template("cefr_analyser.html")

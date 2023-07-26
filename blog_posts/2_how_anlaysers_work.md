cb8521ceedd62df6f6e538ec69ac977fd58045fd
George <george.j.lindley@gmail.com>
2023-07-26 19:37:08 +0300

    with functioning MD  blog

---
+++ blog_posts/2_how_anlaysers_work.md
@@ -0,0 +1,41 @@
+Title: How our Analysers Work
+Date: 2023-07-25
+Author: ChatGPT & George Lindley
+
+We use the magic of AI to unlock the complexities of the GSE (Global Scale of English) and CEFR (Common European Framework of Reference for Languages). Let's look at how are text Analysers work, and provide you with insight on how we achieve accuracy in language analysis.
+
+## GSE Text Analyser - Unlocking the most Comprehensive English Scale:
+
+The Global Scale of English (GSE) is a revolutionary framework that delves into the intricate nuances of language proficiency. Our AI-powered GSE Text Analyser harnesses the capabilities of Natural Language Processing (NLP) and Machine Learning to meticulously assess your written content. By utilizing the latest in AI, we bring you an advanced tool that accurately gauges language complexity.
+
+Our GSE Text Analyser estimates your overall GSE level, combining the Flesch Reading Ease level and the average GSE word scores. The fusion of AI and data-driven analysis provides you with a holistic and futuristic assessment of your language proficiency.
+
+Why the dual approach? See below the critisim of *solely* using the Flesch-Kincaid readability index: https://en.wikipedia.org/wiki/Flesch%E2%80%93Kincaid_readability_tests 
+
+
+![criticism of other scales](../static/blog_images/flesch_criticism.png)
+
+
+Our NLP model meticulously dissects your text, capturing essential linguistic features and comparing each word against an extensive database of over 35,000 words. The result? A comprehensive list of individual word scores that can then be combined with the output of the Flesch-Kincaid index.
+
+## CEFR Text Analyser - Solid Analysers Embracing Language Diversity:
+
+At the heart of global language standards lies the Common European Framework of Reference for Languages (CEFR). Our AI-powered CEFR Text Analyser acknowledges the diverse linguistic landscape by deploying specific readability functions tailored to each supported language.
+
+Whether it's English, German, or any other language we support, our AI thoroughly examines the text, providing an accurate CEFR level that you can trust. The precise and reliable results are a testament to the seamless integration of AI technology with CEFR's well-established language proficiency scale.
+
+## Why Our AI-Powered Text Analysers Stand Out:
+
+1. Precision Redefined:
+Our AI-driven analysers offer unparalleled accuracy, providing you with precise and reliable language assessments. No other text analyser on the internet provides pinpoint GSE scoring.
+
+2. Efficiency at Its Core:
+Experience instant feedback without the hassle of time-consuming evaluations. Our AI ensures efficiency in language analysis.
+
+3. Embracing Multilingualism:
+With support for multiple languages, our website caters to a global audience, promoting language diversity and inclusivity.
+
+
+Join us on this exciting journey of language exploration, where AI empowers you to unlock the GSE and CEFR frameworks, ensuring language proficiency is no longer a mystery.
+
+Experience the Future - Experience Language Proficiency Redefined!
--- website/views.py
+++ website/views.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, request
 import sqlite3
+import os
+import markdown
+from urllib.parse import urljoin
 import textstat
 import spacy
 from helpers import GSELevelEstimator, WordProcessor, CEFRConverter, language_mappings
@@ -98,24 +101,110 @@ class CefrAnalyser(BaseAnalyser):
             if level is not None:
                 cefr_converter = CEFRConverter()
                 cefr = cefr_converter.convert_to_cefr(level, index=index)
-                print(cefr)
                 return cefr, language, index, level
 
         return None, None, None, None
 
+
+BLOG_DIR = "blog_posts"
+
+class Blog:
+    def __init__(self):
+        self.blog_posts = self.generate_blog_post_objects()
+
+    @staticmethod
+    def read_markdown_file(file_path):
+        with open(file_path, "r", encoding="utf-8") as file:
+            return file.read()
+
+    def generate_blog_post_objects(self):
+        blog_posts = []
+
+        for filename in os.listdir(BLOG_DIR):
+            if filename.endswith(".md"):
+                file_path = os.path.join(BLOG_DIR, filename)
+                content = self.read_markdown_file(file_path)
+
+                # Assume that the metadata lines are separated from the content by a double newline
+                metadata_lines, post_content = content.split("\n\n", 1)
+
+                # Remove lines that start with '#' or '##' from the post_content
+                cleaned_post_content = "\n".join(line for line in post_content.splitlines() if not line.strip().startswith('#'))
+
+                # Parse metadata from metadata_lines
+                metadata = {}
+                for line in metadata_lines.splitlines():
+                    key, value = line.split(":", 1)
+                    metadata[key.strip()] = value.strip()
+
+                # Truncate the cleaned_post_content to the first 50 words to use as the excerpt
+                excerpt_words = cleaned_post_content.split()[:50]
+                excerpt = " ".join(excerpt_words) + "..." if len(excerpt_words) >= 50 else cleaned_post_content
+
+                # Convert the Markdown content to HTML
+                html_content = markdown.markdown(post_content)
+
+                # Store the blog post object
+                blog_post = {
+                    "id": len(blog_posts) + 1,  # Assign a unique ID based on the index
+                    "title": metadata.get("Title", "Untitled"),
+                    "date": metadata.get("Date", ""),
+                    "author": metadata.get("Author", ""),
+                    "excerpt": excerpt,
+                    "content": html_content,  # Store the HTML content
+                }
+                blog_posts.append(blog_post)
+
+        return blog_posts
+
+    def get_blog_post_by_id(self, post_id):
+        for blog_post in self.blog_posts:
+            if blog_post["id"] == post_id:
+                # Get image URLs from the raw post content (Markdown)
+                import re
+                image_urls = re.findall(r"!\[[^\]]*\]\((.*?)\)", blog_post["content"])
+
+                # Convert relative image URLs to absolute URLs
+                image_urls = [urljoin(request.base_url, url) for url in image_urls]
+
+                # Add the image URLs to the blog post object
+                blog_post["image_urls"] = image_urls
+
+                return blog_post
+
+        return None
+
+blog_instance = Blog()
 
 @views.route('/')
 def home():
     return render_template("home.html")
 
 @views.route('/blog')
-def blog():
-    return render_template("blog.html")
+def blog_home():
+    blog_posts = blog_instance.blog_posts
+    return render_template("blog.html", blog_posts=blog_posts)
+
+@views.route('/blog/<int:post_id>')
+def blog_post(post_id):
+    blog_post = blog_instance.get_blog_post_by_id(post_id)
+    if blog_post:
+        return render_template("blog_post.html", blog_post=blog_post)
+    else:
+        return "Blog post not found.", 404
+
+from flask import redirect, url_for
 
 @views.route('/gse_analyser', methods=["GET", "POST"])
 def gse_analyser():
     if request.method == "POST":
         data = request.form['text']
+
+        # Check if the text input is empty
+        if not data.strip():
+            # If the text input is empty, redirect back to the same page (GET method)
+            return redirect(url_for('views.gse_analyser'))
+
         gse_analyser = GSEAnalyser()
         overall_gse, overall_cefr, none_words, learn_words = gse_analyser.analyse(data)
 
@@ -131,6 +220,12 @@ def cefr_analyser():
     if request.method == "POST":
         data = request.form['text']
         language = request.form['language']
+        
+        # Check if the text input is empty
+        if not data.strip():
+            # If the text input is empty, redirect back to the same page (GET method)
+            return redirect(url_for('views.cefr_analyser'))
+
         cefr_analyser = CefrAnalyser()
         cefr, language, index, level = cefr_analyser.analyse(data, language)
 

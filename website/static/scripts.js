85ef6c1351d0fd6038a48f20494ddd2c3cedee09
George <george.j.lindley@gmail.com>
2023-08-12 15:51:18 +0300

    new formatting like vitalik

--- website/static/scripts.js
+++ website/static/scripts.js
@@ -1,36 +1,46 @@
 // dark-mode.js
 
-// Check if the user has set a preference for dark mode in the browser
-// If the user has already set a preference, use that preference
-// If not, check the time of day to decide whether to use dark mode or not
-function setInitialDarkMode() {
+// Function to set dark mode and update toggle state
+function setDarkModePreference(enabled) {
   const darkModeToggle = document.getElementById("darkModeToggle");
-
-  // Check if the user's preference is set to dark mode
-  const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-  // Check the time of day (morning or night) to decide whether to use dark mode
-  const hour = new Date().getHours();
-  const isNightTime = hour >= 18 || hour <= 6;
-
-  if (prefersDarkMode || isNightTime) {
-    // Enable dark mode
+  if (enabled) {
     document.body.classList.add("dark-mode");
     darkModeToggle.checked = true;
+    localStorage.setItem("darkMode", "enabled");
+  } else {
+    document.body.classList.remove("dark-mode");
+    darkModeToggle.checked = false;
+    localStorage.setItem("darkMode", "disabled");
   }
 }
 
 // Toggle dark mode when the switch is clicked
 function toggleDarkMode() {
   const darkModeToggle = document.getElementById("darkModeToggle");
-  const isDarkModeEnabled = darkModeToggle.checked;
+  setDarkModePreference(darkModeToggle.checked);
+}
 
-  if (isDarkModeEnabled) {
-    // Enable dark mode
-    document.body.classList.add("dark-mode");
+// Function to set the initial dark mode state
+function setInitialDarkMode() {
+  const darkModeToggle = document.getElementById("darkModeToggle");
+  const storedPreference = localStorage.getItem("darkMode");
+  
+  if (storedPreference === "enabled") {
+    setDarkModePreference(true);
+  } else if (storedPreference === "disabled") {
+    setDarkModePreference(false);
   } else {
-    // Disable dark mode
-    document.body.classList.remove("dark-mode");
+    // Check if the user's preference is set to dark mode
+    const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    // Check the time of day (morning or night) to decide whether to use dark mode
+    const hour = new Date().getHours();
+    const isNightTime = hour >= 18 || hour <= 6;
+    
+    if (prefersDarkMode || isNightTime) {
+      setDarkModePreference(true);
+    } else {
+      setDarkModePreference(false);
+    }
   }
 }
 
